cf32e01cb3bf79832c754f3c10fe89a1acbb6557
inbalchen <inbal6026>
2021-01-02 20:05:12 +0200

    added map icons functionality

--- src/utils/map.js
+++ src/utils/map.js
@@ -1,7 +1,38 @@
+import cloud from '../assets/images/cloud.svg'
+import moon from '../assets/images/moon.svg'
+import sun from '../assets/images/sunny.svg'
+import partlysun from '../assets/images/sun-part-most.svg'
+import partlynight from '../assets/images/part-night.svg'
+import rain from '../assets/images/rain.svg'
+
 export const Days = {
     '0': 'Sunday',
     '1': 'Monday',
     '2': 'Tuesday',
     '3': 'Wednesday',
     '4': 'Thursday'
+}
+
+export const dayIcons = (condition) => {
+    if(condition.toLowerCase().includes('rain')){
+        return rain
+    }else if(condition.toLowerCase().includes('part') || condition.toLowerCase().includes('most') || condition.toLowerCase().includes('inter')){
+        return partlysun
+    }else if(condition.toLowerCase().includes('cloud')){
+        return partlysun
+    }else if(condition.toLowerCase().includes('sun')){
+        return sun
+    }
+}
+
+export const nightIcons = (condition) => {
+    if(condition.toLowerCase().includes('rain')){
+        return rain
+    }else if(condition.toLowerCase().includes('part') || condition.toLowerCase().includes('most') || condition.toLowerCase().includes('inter')){
+        return partlynight
+    }else if(condition.toLowerCase().includes('clear')){
+        return moon
+    }else if(condition.toLowerCase().includes('cloud')){
+        return cloud
+    }
 }
